f34accad30c0422a8a89ab8e487fb0f2db47db0e
nickshirsat <nikshirsat007@gmail.com>
2021-05-25 17:23:21 +0530

    2 cases solved

--- src/main/java/com/sb/demo/OrderController.java
+++ src/main/java/com/sb/demo/OrderController.java
@@ -1,7 +1,12 @@
 package com.sb.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +18,16 @@ public class OrderController {
 	OrderRepo repo;
 	
 	@PostMapping("/save-order")
-	public void saveOrder(@ModelAttribute(name = "Order") Order order) {
+	public String saveOrder(@ModelAttribute(name = "Order") Order order) {
 		repo.save(order);
 		System.out.println("Data saved!");
-		System.out.println(order);
+		return order.toString();
+	}
+	
+	@GetMapping("get-order/{id}")
+	public String getOrder(@PathVariable Integer id) {
+		Optional<Order> order = repo.findById(id);
+		System.out.println(id);
+		return order.toString();
 	}
 }
